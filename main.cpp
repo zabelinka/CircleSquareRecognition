9443ad5d55e5b241893660143941885e5a7f9425
Tatiana Zabelina <zzabelinka@gmail.com>
2018-06-13 23:28:04 +0300

    clear code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <string>
 
 #include <opencv2/imgproc.hpp>
 #include <opencv/highgui.h>
-
-
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -35,55 +32,52 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    cv::Mat labelImage, my_stat, centroid;
-    int nLabels = cv::connectedComponentsWithStats(binary, labelImage, my_stat, centroid, 8);
+    std::cout << "Source image = " << std::endl << binary << std::endl;
+
+    cv::Mat labeled, statistics, centroid;
+    const int number_of_labels = cv::connectedComponentsWithStats(binary, labeled, statistics, centroid, 8);
 
     cv::Rect biggest_bb;
 
     // skip background label 0
-    for (int i = 1; i < nLabels; i++) {
-        cv::Size size(my_stat.at<int>(i, cv::CC_STAT_WIDTH ), my_stat.at<int>(i, cv::CC_STAT_HEIGHT));
+    for (int i = 1; i < number_of_labels; ++i) {
+        const cv::Size size(statistics.at<int>(i, cv::CC_STAT_WIDTH), statistics.at<int>(i, cv::CC_STAT_HEIGHT));
 
-        if (size.width > biggest_bb.width || size.height > biggest_bb.height){
-            cv::Point corner(my_stat.at<int>(i, cv::CC_STAT_LEFT ), my_stat.at<int>(i,cv::CC_STAT_TOP));
+        if (size.width > biggest_bb.width || size.height > biggest_bb.height) {
+            const cv::Point corner(statistics.at<int>(i, cv::CC_STAT_LEFT), statistics.at<int>(i,cv::CC_STAT_TOP));
             biggest_bb = cv::Rect(corner, size);
         }
     }
 
-    std::cout << "Found biggest bounding box: ("
-              << biggest_bb.x << ", " << biggest_bb.y << ", "
-              << biggest_bb.width << ", " << biggest_bb.height << ")" << std::endl;
+    std::cout << "Found biggest bounding box: " << biggest_bb << std::endl;
 
     if (biggest_bb.width != biggest_bb.height) {
-        std::cout << "Unknown shape. Bounding box has different width and height\n";
+        std::cout << "Unknown shape. Bounding box has different width and height." << std::endl;
         return 0;
     }
     if (biggest_bb.width < 5) {
         std::cout << "Unknown shape. Width is too small to be valid. Actual: "
-                     << biggest_bb.width << ", expected greater than 5" << std::endl;
+                  << biggest_bb.width << ", expected greater than 5." << std::endl;
         return 0;
     }
     if (biggest_bb.width > 10) {
         std::cout << "Unknown shape. Width is too big to be valid. Actual: "
-                  << biggest_bb.width << ", expected less than 10" << std::endl;
+                  << biggest_bb.width << ", expected less than 10." << std::endl;
         return 0;
     }
 
-    std::cout << "Origin = " << std::endl << binary << std::endl;
-
     uchar kernel_data[9] = {0, 1, 0, 1, 1, 1, 0, 1, 0};
-    cv::Mat kernel = cv::Mat(3, 3, CV_8UC1, kernel_data);
-    std::cout << "Kernel = " << std::endl << kernel << std::endl;
+    const cv::Mat kernel = cv::Mat(3, 3, CV_8UC1, kernel_data);
 
     cv::Mat opened = cv::Mat::zeros(binary.size(), CV_8UC1);
     morphologyEx(binary, opened, cv::MORPH_OPEN, kernel);
-    std::cout << "Opened = " << std::endl << opened << std::endl;
+    std::cout << "Opened image = " << std::endl << opened << std::endl;
 
     cv::Mat binary_bb = binary(biggest_bb);
     cv::Mat opened_bb = opened(biggest_bb);
 
-    cv::Mat diff = binary_bb - opened_bb;
-    std::cout << "Diff = " << std::endl << diff << std::endl;
+    const cv::Mat diff = binary_bb - opened_bb;
+    std::cout << "Diff image = " << std::endl << diff << std::endl;
 
     std::vector<cv::Point> diff_points;
     for (int y = 0; y < diff.rows; ++y) {
